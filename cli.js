1e48ccdab1d9cf6fb8d90452e65b9fdc9a58c31a
Danny Morgan <dan@dannydev.com>
2018-01-15 21:17:34 -0500

    Add an optional pattern argument to cli

--- cli.js
+++ cli.js
@@ -3,10 +3,18 @@
 const program = require('commander');
 const pkg = require('./package.json');
 
+// cli
 program
   .version(pkg.version)
-  .arguments('<glob>')
   .description('Convert from SVG to PNG with Chrome Puppeteer')
+  .option(
+    '--pattern [glob]',
+    'Glob pattern to match files against',
+    'src/**/*.svg'
+  )
   .option('--width [width]', 'Specify the output png width at 1x', 32)
   .option('--height [height]', 'Specify the output png height at 1x', 32)
   .parse(process.argv);
+
+// output help if nothing is set
+if (!process.argv.slice(2).length) program.help();
