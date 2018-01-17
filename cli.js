b36f92f193785a8f3f7db66858ffa6f59af6d434
Danny Morgan <dan@dannydev.com>
2018-01-16 20:28:41 -0500

    CLI in progress, generates scripts and preserves folder structure

--- cli.js
+++ cli.js
@@ -1,20 +1,31 @@
 #!/usr/bin/env node
 
 const program = require('commander');
+const chalk = require('chalk');
 const pkg = require('./package.json');
+const app = require('./');
 
 // cli
 program
   .version(pkg.version)
   .description('Convert from SVG to PNG with Chrome Puppeteer')
+  .arguments('<files>')
+  .option('--width <width>', 'Specify the output png width at 1x', 32)
+  .option('--height <height>', 'Specify the output png height at 1x', 32)
   .option(
-    '--pattern [glob]',
-    'Glob pattern to match files against',
-    'src/**/*.svg'
+    '--output <output>',
+    'Specify the output directory for generated files',
+    './pup4svg2png'
   )
-  .option('--width [width]', 'Specify the output png width at 1x', 32)
-  .option('--height [height]', 'Specify the output png height at 1x', 32)
-  .parse(process.argv);
+  .action(app);
+
+function exitWithHelp(msg) {
+  program.outputHelp();
+  console.error('\n\n', chalk.red(msg), '\n\n');
+  process.exit(1);
+}
 
 // output help if nothing is set
-if (!process.argv.slice(2).length) program.help();
+program.parse(process.argv);
+if (!process.argv.slice(2).length)
+  exitWithHelp('Please specify a glob pattern to search for');
